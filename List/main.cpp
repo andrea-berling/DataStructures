4faf110cbc01c303ce782ce32be9840cf994111b
andrea-berling <berlingieri.andrea@gmail.com>
2017-07-15 21:50:25 +0200

    trivial changes

--- List/main.cpp
+++ List/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
 	List<int> l;
-    List<int>::iterator it;
-	for(int i = 0; i < 100; i++)
+    List_iterator<int> it;
+	for(int i = 0; i < 10; i++)
 		l.insert(i);
     for(it = l.begin(); it != l.end(); it++)
 	{
@@ -16,13 +16,10 @@ int main()
 
     cout << endl;
 
-    for(it = --l.end(); it != --l.begin(); it--)
+    for(it = --l.end(); it != l.end(); it--)
 	{
 		cout << *it << " ";
 	}
 	cout << endl;
 
-	for(int i = 0; i < 100; i++)
-        cout << l.contains(i+1) << " " ;
-    cout << endl;
 }
